41081f0312fb39e3d2da28f53816f75fca48ae54
Andrew Brereton <andrew@brereton.email>
2026-05-15 17:36:00 +1000

    Upgrade version from 2.2.1 to 2.2.2

--- src/IcalService.ts
+++ src/IcalService.ts
@@ -15,7 +15,7 @@ export class IcalService {
     const calendar = '' +
       'BEGIN:VCALENDAR\r\n' +
       'VERSION:2.0\r\n' +
-      'PRODID:-//Andrew Brereton//obsidian-ical-plugin v2.2.1//EN\r\n' +
+      'PRODID:-//Andrew Brereton//obsidian-ical-plugin v2.2.2//EN\r\n' +
       'X-WR-CALNAME:Obsidian Calendar\r\n' +
       'NAME:Obsidian Calendar\r\n' +
       'CALSCALE:GREGORIAN\r\n' +
